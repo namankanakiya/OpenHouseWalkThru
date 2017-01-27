5305cf6f1ed9ed7a7b5317b5ced9c62a159e4a61
nafiz007 <nafizz1407@gmail.com>
2017-01-27 18:30:36 -0500

    Added links to go back to Dashboard from Add House page.

--- app/components/AddHouse.jsx
+++ app/components/AddHouse.jsx
@@ -1,26 +1,37 @@
 var React = require('react');
 
 var AddHouse = React.createClass({
-  render: function() {
-    return (
-    	<div >
-    		<form>
-                Address:<br/>
-	            <input type="text" name="address" /><br/>
-	            City:<br/>
-	            <input type="text" name="city" /> <br/>
-	            State:<br/>
-	            <input type="text" name="State" /><br/>
-	            ZipCode:<br/>
-	            <input type="text" name="zipcode" /><br/>
-	            Description:<br/>
-	            <input type="text" name="description" /><br/><br/>
-	            <p><a href="/">Add House</a></p>
-	            <input type="button" value="Upload Pictures"/>
-	        </form>
-    	</div>       
-    )
-   } 
+    render: function() {
+        return (
+            <div>
+                <form action="/">
+                    <label>
+                        Address:
+                        <input type="text" name="address" />
+                    </label>
+                    <label>
+                        City:
+                        <input type="text" name="city" />
+                    </label>
+                    <label>
+                        State:
+                        <input type="text" name="State" />
+                    </label>
+                    <label>
+                        ZipCode:
+                        <input type="text" name="zipcode" />
+                    </label>
+                    <label>
+                        Description:
+                        <input type="text" name="description" />
+                    </label>
+                    <button type="submit" className="button primary">Add House</button>
+                    <button className="button primary">Upload Photos</button>
+                </form>
+                <a href="/"><button className="button secondary">Back</button></a>
+            </div>
+        )
+    }
 });
 
 module.exports = AddHouse;
--- app/components/Checklist.jsx
+++ app/components/Checklist.jsx
@@ -1,43 +1,42 @@
 var React = require('react');
 
-
 var Checklist = React.createClass({
-	getInitialState : function() {
-		return {
-			features : ["Bedroom", "Driveway", "Kitchen"]
-		};
-	},
+    getInitialState : function() {
+        return {
+            features : ["Bedroom", "Driveway", "Kitchen"]
+        };
+    },
 
-	addFeature : function(e) {
-		e.preventDefault();	
-		var newFeature = this.refs.newFeature.value;
-		if (newFeature.length > 0) {
-			this.refs.newFeature.value = '';
-			this.setState(
-				{features : [...this.state.features, newFeature]});
-		}
-	},
+    addFeature : function(e) {
+        e.preventDefault(); 
+        var newFeature = this.refs.newFeature.value;
+        if (newFeature.length > 0) {
+            this.refs.newFeature.value = '';
+            this.setState(
+                {features : [...this.state.features, newFeature]});
+        }
+    },
 
-	render : function() {
-		var key = 0;
-		return (
-			<div className="checklist-main">
-				<ul>
-					{this.state.features.map((feature) => {
-						return <li key={key++}>{feature}</li>
-					})}
-				</ul>
-				<br/>
-				<div>
-					<form onSubmit={this.addFeature} className="sameLine">
-						<input type="text" ref="newFeature" placeholder ="Enter a feature for tracking"/>
-						<button className="button primary"> Add Feature </button>
-					</form>
-					<a href="/#/houseprofile"><button className="back button secondary">Go back</button></a>
-				</div>
-			</div>
-		);
-	}
+    render : function() {
+        var key = 0;
+        return (
+            <div className="checklist-main">
+                <ul>
+                    {this.state.features.map((feature) => {
+                        return <li key={key++}>{feature}</li>
+                    })}
+                </ul>
+                <br/>
+                <div>
+                    <form onSubmit={this.addFeature} className="sameLine">
+                        <input type="text" ref="newFeature" placeholder ="Enter a feature for tracking"/>
+                        <button className="button primary"> Add Feature </button>
+                    </form>
+                    <a href="/#/houseprofile"><button className="back button secondary">Go back</button></a>
+                </div>
+            </div>
+        );
+    }
 })
 
 module.exports = Checklist;
