7d771fe7987ae9167b873b38eddd5553ff023c95
Anurag <v.anuragsharma@gmail.com>
2017-02-08 11:31:40 -0500

    Removed search bar and added a dropdown menu in the Nav Bar.

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,5 +1,61 @@
 var React = require('react');
 
+var NavMenu = React.createClass({
+    getDefaultProps : function() {
+        return {
+            isOpen : false
+        };
+    },
+
+    render : function() {
+        if (this.props.isOpen) {
+            return (
+                <div className="dropdown">
+                    <ul>
+                        <li><a href="/#/userprofile">Profile</a></li>
+                        <li><a href="/#/settings">Settings</a></li>
+                        <li><a href="/#/logout">Logout</a></li>
+                    </ul>
+                </div>
+            );
+        }
+        return null;
+    }
+});
+
+var NavMenuButton = React.createClass({
+
+    getInitialState : function() {
+        return {
+            isOpen : false
+        };
+    },
+
+    handleClick : function(e) {
+        e.stopPropagation();
+        console.log("button click", this.state.isOpen);
+        this.setState({isOpen: !this.state.isOpen});
+    },
+
+    componentDidMount : function() {},
+
+    componentWillUnmount : function() {},
+
+    handleBodyClick : function(e) {
+        this.setState({isOpen : false});
+    },
+
+    render : function() {
+        return (
+            <div onClick={this.handleBodyClick}>
+                <a onClick={this.handleClick} href="#">Menu</a>
+                <NavMenu isOpen={this.state.isOpen} />
+            </div>
+        );
+    }
+
+});
+
 var Nav = React.createClass({
     render : function() {
         return (
@@ -8,16 +64,10 @@ var Nav = React.createClass({
                     <ul className="dropdown menu" data-dropdown-menu>
                         <li className="menu-text">Open House Walk Thru</li>
                         <li><a href="/#/">Dashboard</a></li>
-                        <li><a href="/#/userprofile">Profile</a></li>
-                        <li><a href="/#/settings">Settings</a></li>
-                        <li><a href="/#/logout">Logout</a></li>
                     </ul>
                 </div>
                 <div className="top-bar-right">
-                    <ul className="menu">
-                        <li><input type="search" placeholder="Search"/></li>
-                        <li><button type="button" className="button">Search</button></li>
-                    </ul>
+                    <NavMenuButton />
                 </div>
             </div>
         );
