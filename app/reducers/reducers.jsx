5497160d65cbabebc72c57479fb637bfb7793f3f
Naman Kanakiya <naman.kanakiya@gmail.com>
2017-02-11 23:30:11 -0500

    removed unncessary reducer

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,23 +1,6 @@
 var uuid = require('node-uuid');
 var ohwtAPI = require('ohwtAPI');
 
-export var checklistReducer = (state = [], action) => {
-	switch (action.type) {
-		case 'ADD_CHECKLIST_ITEM':
-			return [
-				...state,
-				{
-					id : uuid(),
-					feature : action.feature
-				}
-			];
-		case 'DELETE_CHECKLIST_ITEM':
-			return state.filter(feature => feature.id != action.id);
-		default:
-			return state;
-	};
-};
-
 export var housesReducer = (state = [], action) => {
 	switch (action.type) {
 		case 'ADD_HOUSE':
--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -4,7 +4,6 @@ import { reducer as formReducer } from 'redux-form'
 
 export var configure = () => {
 	var reducer = redux.combineReducers({
-		checklist : checklistReducer,
 		form : formReducer,
 		houses : housesReducer
 	});
