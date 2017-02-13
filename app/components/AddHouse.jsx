f04ed908a48b9f3ef7bb2a97de6a8e1c721d568f
Naman Kanakiya <naman.kanakiya@gmail.com>
2017-02-13 10:35:43 -0500

    removed old form from addhouse

--- app/components/AddHouse.jsx
+++ app/components/AddHouse.jsx
@@ -36,37 +36,6 @@ var AddHouse = React.createClass({
         this.props.router.push('/');
     },
     render: function() {
-        var oldBody = (<div>
-                <form onSubmit={this.addHouse}>
-                    <label>
-                        Address:
-                        <input type="text" name="address" ref="address"/>
-                    </label>
-                    <label>
-                        City:
-                        <input type="text" name="city" ref="city"/>
-                    </label>
-                    <label>
-                        State:
-                        <input type="text" name="State" ref="state"/>
-                    </label>
-                    <label>
-                        ZipCode:
-                        <input type="text" name="zipcode" ref="zipcode"/>
-                    </label>
-                    <label>
-                        Description:
-                        <input type="text" name="description" ref="description"/>
-                    </label>
-                    <label>
-                        Image URL:
-                        <input type="text" name="imageurl" ref="imageurl"/>
-                    </label>
-                    <button type="submit" className="button primary">Add House</button>
-                    {/*<button className="button primary" type="button">Upload Photos</button>*/}
-                </form>
-                <Link to="/"><button className="button secondary">Back</button></Link>
-            </div>);
         return (
             <SyncValidationForm onSubmit={this.addHouse}/>
         )
