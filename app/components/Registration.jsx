98a0ab95979aa1eb3102cc95ff1cb2b12c882d26
Anurag <v.anuragsharma@gmail.com>
2017-03-27 00:29:43 -0400

    Methods to handle login and registration have been written. Still in progress...

--- app/components/Registration.jsx
+++ app/components/Registration.jsx
@@ -2,6 +2,35 @@ var React = require('react');
 
 var Registration = React.createClass({
 
+    {/*function handleSignUp() {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      // Sign in with email and pass.
+      // [START createwithemail]
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+      // [END createwithemail]
+    }*/}
+
     render : function() {
 
     var mainContainer = {
